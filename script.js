e554db737f2eeb2e3792fe5a86cff045a4c09950
rqpt <1ernstvermeulen1@gmail.com>
2022-10-23 17:11:07 +0200

    do over

--- script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display p');
-console.log(buttons);
-console.log(display);
-buttons.forEach(button => button.addEventListener('click', takeNum));
-
-function takeNum(
-
