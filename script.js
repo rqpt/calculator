850e13fb7781ef9a70d81c981455982c0bcf4c00
rqpt <1ernstvermeulen1@gmail.com>
2022-10-25 00:02:17 +0200

    Write functions for operator logic and operate to call said functions

---
+++ script.js
@@ -0,0 +1,35 @@
+// Operator functions
+
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+// Operate function
+
+function operate(operator, num1, num2) {
+	switch (operator) {
+		case add:
+			return add(num1, num2);
+			break;
+		case subtract:
+			return subtract(num1, num2);
+			break;
+		case multiply:
+			return multiply(num1, num2);
+			break;
+		case divide:
+			return divide(num1, num2);
+	}
+}
