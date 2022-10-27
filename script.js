f36c83334f886acda48a97aaca3928cd6a732a5a
rqpt <1ernstvermeulen1@gmail.com>
2022-10-27 12:34:53 +0200

    to continue at work

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const display = document.querySelector('p');
-const inputs = document.querySelectorAll('button');
+const regInputs = document.querySelectorAll('.button.regular');
+const specialInputs = document.querySelectorAll('button.special');
 
-inputs.forEach(input.addEventListener('click', (e) => {
-	console.log(e);
+regInputs.forEach(input => input.addEventListener('click', e => {
+	displayVal(e.target.innerText);
 }));
+
+// Display function
+
+function displayVal(value) {
+	const operands = ['x', '/', '-', '+',]
+	if (operands.find(operand => operand === value) && display.innerText === '') {
+		display.innerText = 'ERROR';
+	} else {
+		display.innerText = '';
+		display.innerText += value;
+	}
+};
+
+// Calculate function
+
+// if the display was clear and user inputs a number
+// create a new object called calculation with the first input being a property of firstnum
+// for each additional number input thereafter append the firstnum property with said number
+// until user inputs an operator
+// when user inputs an operator, create a new property for the calculation object called operator and store the input operator as it's value
+// if the user inputs another operator, replace the operator property with a the new one
+// until user inputs another number
+// when user inputs a number, repeat the firstnum property logic but with a new property called secondnum
+// until user inputs another operator or equals
+// if user inputs another operator
+// store the result of the in the firstnum property and display it on the calculator display
+// and store the operator in the operator property
+// else just store the result in the fistnum property and display it
+// if user presses poweroff, all the object is removed
+// else if user inputs undo, the move back to the previous step?
+
+function calculate ()
 
 // Operator functions
 
